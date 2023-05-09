977ab6c6d833bc20b935d4ce89e46473af682c58
Adam.B3 <readingfootyclub1871@gmail.com>
2023-05-09 12:14:01 +0100

    Initial commit

---
+++ cw2/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+# PAYMENT PROVIDERS
+
+class PersonalAccount(models.Model):
+    accountNumber = models.IntegerField(primary_key=True)
+    paymentDetails = models.ForeignKey('PaymentDetails', on_delete=models.CASCADE)
+    bankDetails = models.OneToOneField("BankDetails", on_delete=models.CASCADE)
+    email = models.TextField(max_length=60)
+    password = models.TextField(max_length=256)
+    phoneNumber = models.TextField(max_length=13)
+    fullName = models.TextField(max_length=80)
+
+
+class BusinessAccount(models.Model):
+    accountNumber = models.IntegerField(primary_key=True)
+    paymentDetails = models.ForeignKey('PaymentDetails', on_delete=models.CASCADE)
+    bankDetails = models.OneToOneField("BankDetails", on_delete=models.CASCADE)
+    businessNumber = models.IntegerField()
+    businessName = models.TextField(max_length=40)
+    businessEmail = models.TextField(max_length=50)
+    businessPhoneNumber = models.TextField(max_length=13)
+
+
+class Transaction(models.Model):
+    id = models.IntegerField(primary_key=True)
+    payer = models.ForeignKey('PersonalAccount', on_delete=models.CASCADE)
+    payee = models.ForeignKey('BusinessAccount', on_delete=models.CASCADE)
+    amount = models.FloatField()
+    currency = models.TextField()
+    date = models.DateTimeField()
+    transactionStatus = models.TextField()
+
+
+class PaymentDetails(models.Model):
+    paymentId = models.IntegerField(primary_key=True)
+    cardNumber = models.TextField()
+    securityCode = models.TextField()
+    expiryDate = models.DateTimeField()
+
+
+class BankDetails(models.Model):
+    accountNumber = models.IntegerField(primary_key=True)
+    sortCode = models.TextField()
+    accountName = models.TextField()
