94fc5401af34016bb0b7448bfff587f42a589d32
Adam.B3 <readingfootyclub1871@gmail.com>
2023-05-10 14:44:09 +0100

    all validation for initiate payment completed

--- cw2/views.py
+++ cw2/views.py
@@ -40,9 +40,6 @@ def InitiatePayment(request):
     if bodyStatus is not None:
         return bodyStatus
 
-    # the data being stored in the database, of any conversions are required they're stored here
-    modelData = {}
-
     # check all fields fit their specific criteria
 
     # in case of spaces in credit card number
@@ -52,12 +49,12 @@ def InitiatePayment(request):
         return errorHandling(104, "CardNumber")
 
     # check made up of only numbers and is 3-4 characters long
-    if not data["CVV"].isdigit() or len(data["CVV"]) not in (3,4):
+    if not data["CVV"].isdigit() or len(data["CVV"]) not in (3, 4):
         return errorHandling(104, "CVV")
 
     # check expiry is in date format
     try:
-        modelData["Expiry"] = datetime.strptime(data["Expiry"], '%Y-%m-%d').date()
+        data["Expiry"] = datetime.strptime(data["Expiry"], '%Y-%m-%d').date()
     except:
         return errorHandling(104, "Expiry")
 
@@ -74,8 +71,8 @@ def InitiatePayment(request):
         return errorHandling(104, "PayeeBankAccNum")
 
     # remove any - separators and ensure length is 6
-    modelData["PayeeBankSortCode"] = data["PayeeBankSortCode"].replace("-", "")
-    if not modelData["PayeeBankSortCode"].isdigit() or len(modelData["PayeeBankSortCode"]) !=6:
+    data["PayeeBankSortCode"] = data["PayeeBankSortCode"].replace("-", "")
+    if not data["PayeeBankSortCode"].isdigit() or len(data["PayeeBankSortCode"]) != 6:
         return errorHandling(104, "PayeeBankSortCode")
 
     # Check card-holder name isn't too long
@@ -86,22 +83,54 @@ def InitiatePayment(request):
     if data["Amount"] < 0:
         return errorHandling(104, "Amount")
 
-    # check that the personal account of the payer exists
-    personalData = PaymentDetails.objects.filter(cardNumber=data["CardNumber"],
-                                           securityCode=data["CVV"]).values()
-
+    # check that the payment account of the payer exists
+    allPaymentData = PaymentDetails.objects.filter(cardNumber=data["CardNumber"],
+                                                   securityCode=data["CVV"]).values()
     # incorrect card number of CVV
-    if len(personalData) != 1:
-        return errorHandling(108)
+    if len(allPaymentData) != 1:
+        return errorHandling(106)
+
+    # queryset can be condensed
+    paymentData = allPaymentData[0]
 
     # incorrect expiry date
-    if personalData[0]["expiryDate"].date() != modelData["Expiry"]:
+    if paymentData["expiryDate"].date() != data["Expiry"]:
+        return errorHandling(106)
+
+    # check that the payee details provided match those linked to the bank account
+    allPayerData = PersonalAccount.objects.filter(paymentDetails=paymentData["paymentId"]).all()
+
+    # personal account does not exist
+    if len(allPayerData) != 1:
         return errorHandling(108)
 
+    # queryset can be condensed
+    payerData = allPayerData[0]
 
+    # make sure all other fields for personal accounts match
+    if payerData.fullName != data["CardHolderName"] or payerData.email != data["Email"]:
+        return errorHandling(108)
 
-    # check that the payee exists with the correct bank details
+    # make sure that the payee details match those of a registered business account and
+    # that the corresponding bank details also match
 
+    allBankData = BankDetails.objects.filter(accountNumber=data["PayeeBankAccNum"],
+                                             sortCode=data["PayeeBankSortCode"],
+                                             accountName=data["RecipientName"]).all()
+    # if no bank details found
+    if len(allBankData) != 1:
+        return errorHandling(107)
+
+    # see if bank corresponds to business account
+    businessData = BusinessAccount.objects.filter(bankDetails=allBankData[0].accountNumber).all()
+
+    # if no corresponding account
+    if len(businessData) != 1:
+        return errorHandling(109)
+
+    # if other details don't also match
+    if businessData[0].businessName != data["RecipientName"]:
+        return errorHandling(109)
 
     currencyData = {"CurrencyFrom": data["PayerCurrencyCode"], "CurrencyTo": data["PayeeCurrencyCode"],
                     "Date": str(date.today()), "Amount": data["Amount"]}
@@ -193,7 +222,6 @@ def ConvertCurrency(data):
     # content = response.content
 
     content = {"Status": 200, "Error code": 123, "Amount": 120.00}
-
 
     return content
 
